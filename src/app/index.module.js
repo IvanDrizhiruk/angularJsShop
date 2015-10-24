2753b453bae86fb3ad960071223585d393d7a616
ivan.drizhiruk <ivan.drizhiruk@gmail.com>
2015-10-24 12:35:34 +0300

    Pages has added.

--- src/app/index.module.js
+++ src/app/index.module.js
@@ -1,7 +1,17 @@
 (function() {
   'use strict';
 
-  angular
-    .module('striver', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria', 'ngResource', 'ngRoute', 'ngMaterial', 'toastr']);
+  angular.module('striver',
+      [
+        'ngAnimate',
+        'ngCookies',
+        'ngTouch',
+        'ngSanitize',
+        'ngMessages',
+        'ngAria',
+        'ngResource',
+        'ngRoute',
+        'ngMaterial',
+        'toastr']);
 
 })();
--- src/app/index.route.js
+++ src/app/index.route.js
@@ -7,13 +7,27 @@
 
   function routeConfig($routeProvider) {
     $routeProvider
-      .when('/', {
+      .when('/home', {
         templateUrl: 'app/main/main.html',
         controller: 'MainController',
         controllerAs: 'main'
       })
+      .when('/contactas', {
+        templateUrl: 'app/contactas/contactas.html'
+      })
+      .when('/about', {
+        templateUrl: 'app/about/about.html'
+      })
+      .when('/orders', {
+        templateUrl: 'app/orders/orders.html'
+      })
+      .when('/example', {
+        templateUrl: 'app/main/example/main.html',
+        controller: 'MainExampleController',
+        controllerAs: 'main'
+      })
       .otherwise({
-        redirectTo: '/'
+        redirectTo: '/home'
       });
   }
 
