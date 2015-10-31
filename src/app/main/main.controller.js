05948eab5d9da421a2053a1d459c34f5fa4ead08
ivan.drizhiruk <ivan.drizhiruk@gmail.com>
2015-10-31 16:33:21 +0200

    AddImage preview

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -12,6 +12,11 @@
     vm.allCategories = goods.allCategories;
     vm.goodsByCategory = goods.goodsByCategory;
     vm.navigateTo = navigateTo;
+    vm.viewTypes= {
+      CUBE: 'cube',
+      LINE: 'lines'
+    };
+    vm.viewType = vm.viewTypes.CUBE;
 
     function navigateTo(good) {
       $location.path("goods/" + good.id);
